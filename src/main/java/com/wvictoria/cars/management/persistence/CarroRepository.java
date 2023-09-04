842953d395a3514afe14a499780b905d39021018
wvictoriadev <wvictoriadev@gmail.com>
2023-09-03 22:30:58 -0500

    :computer: Implementacion obtener Carro por Id

--- src/main/java/com/wvictoria/cars/management/persistence/CarroRepository.java
+++ src/main/java/com/wvictoria/cars/management/persistence/CarroRepository.java
@@ -2,13 +2,19 @@ package com.wvictoria.cars.management.persistence;
 
 import com.wvictoria.cars.management.persistence.crud.CarroCrudRepository;
 import com.wvictoria.cars.management.persistence.entity.Carro;
+import com.wvictoria.cars.management.persistence.entity.Empleado;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CarroRepository {
     private CarroCrudRepository carroCrudRepository;
 
     public List<Carro> getAll() {
         return (List<Carro>) carroCrudRepository.findAll();
+    }
+
+    public Optional<Carro> getCarro(int idCarro) {
+        return  carroCrudRepository.findById(idCarro);
     }
 }
