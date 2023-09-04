3f3fba4639585bd7086c85f2f215277bdc6ba8d9
wvictoriadev <wvictoriadev@gmail.com>
2023-09-03 23:52:46 -0500

    :computer: Implementacion TripService

---
+++ src/main/java/com/wvictoria/cars/management/domain/service/TripService.java
@@ -0,0 +1,38 @@
+package com.wvictoria.cars.management.domain.service;
+
+import com.wvictoria.cars.management.domain.Trip;
+import com.wvictoria.cars.management.domain.repository.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TripService {
+    @Autowired
+    private TripRepository tripRepository;
+
+    public List<Trip> getAll() {
+        return tripRepository.getAll();
+    }
+
+    public Optional<Trip> getTrip(int tripId) {
+        return tripRepository.getTrip(tripId);
+    }
+
+    public Optional<List<Trip>> getByEmployee(int employeeId) {
+        return tripRepository.getByEmployee(employeeId);
+    }
+
+    public Trip save(Trip trip) {
+        return tripRepository.save(trip);
+    }
+
+    public boolean delete(int tripId) {
+        return getTrip(tripId).map(trip -> {
+            tripRepository.delete(tripId);
+            return true;
+        }).orElse(false);
+    }
+}
