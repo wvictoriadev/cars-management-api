00e570265b8a67ced96a44f44bffc9daa78c46b7
wvictoriadev <wvictoriadev@gmail.com>
2023-09-03 22:29:11 -0500

    :computer: Implementacion obtener Empleado por Id

--- src/main/java/com/wvictoria/cars/management/persistence/EmpleadoRepository.java
+++ src/main/java/com/wvictoria/cars/management/persistence/EmpleadoRepository.java
@@ -4,11 +4,16 @@ import com.wvictoria.cars.management.persistence.crud.EmpleadoCrudRepository;
 import com.wvictoria.cars.management.persistence.entity.Empleado;
 
 import java.util.List;
+import java.util.Optional;
 
 public class EmpleadoRepository {
     private EmpleadoCrudRepository empleadoCrudRepository;
 
     public List<Empleado> getAll() {
         return (List<Empleado>) empleadoCrudRepository.findAll();
+    }
+
+    public Optional<Empleado> getEmpleado(int idEmpleado) {
+        return  empleadoCrudRepository.findById(idEmpleado);
     }
 }
