6be51984eab4857b882f10e8a25a3c52609caaef
wvictoriadev <wvictoriadev@gmail.com>
2023-09-03 23:52:30 -0500

    :computer: Implementacion metodos ViajeRepository

--- src/main/java/com/wvictoria/cars/management/persistence/ViajeRepository.java
+++ src/main/java/com/wvictoria/cars/management/persistence/ViajeRepository.java
@@ -1,16 +1,51 @@
 package com.wvictoria.cars.management.persistence;
 
+import com.wvictoria.cars.management.domain.Trip;
+import com.wvictoria.cars.management.domain.repository.TripRepository;
 import com.wvictoria.cars.management.persistence.crud.ViajeCrudRepository;
 import com.wvictoria.cars.management.persistence.entity.Viaje;
+import com.wvictoria.cars.management.persistence.mapper.TripMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public class ViajeRepository {
+public class ViajeRepository implements TripRepository {
+    @Autowired
     private ViajeCrudRepository viajeCrudRepository;
+    @Autowired
+    private TripMapper tripMapper;
+    public List<Trip> getAll() {
+        List<Viaje> viajes = (List<Viaje>) viajeCrudRepository.findAll();
+        return tripMapper.toTrips(viajes);
+    }
 
-    public List<Viaje> getAll() {
-        return (List<Viaje>) viajeCrudRepository.findAll();
+    @Override
+    public Optional<List<Trip>> getByEmployee(int employeeId) {
+        List<Viaje> viajes = viajeCrudRepository.findByIdEmpleadoOrderByNombreAsc(employeeId);
+        return Optional.of(tripMapper.toTrips(viajes));
+    }
+
+    @Override
+    public Optional<List<Trip>> getByCar(int carId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Trip> getTrip(int tripId) {
+        return viajeCrudRepository.findById(tripId).map(viaje -> tripMapper.toTrip((viaje)));
+    }
+
+    @Override
+    public Trip save(Trip trip) {
+        Viaje viaje = tripMapper.toViaje(trip);
+        return tripMapper.toTrip(viajeCrudRepository.save(viaje));
+    }
+
+    @Override
+    public void delete(int tripId) {
+        viajeCrudRepository.deleteById(tripId);
     }
 }
