17b678595402ef82c831dc8551ff58e70895ba6b
wvictoriadev <wvictoriadev@gmail.com>
2023-09-03 21:56:16 -0500

    :tada: Initial Commit

---
+++ src/main/java/com/wvictoria/cars/management/CarsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wvictoria.cars.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvictoria/cars/management/CarsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvictoria.cars.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
