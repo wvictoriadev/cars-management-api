0c13186e427a146a14fbf5fc9229d34bd0bca4df
wvictoriadev <wvictoriadev@gmail.com>
2023-09-03 23:51:07 -0500

    :computer: Encontrar lista de Viajes asociado a un Empleado

--- src/main/java/com/wvictoria/cars/management/persistence/crud/ViajeCrudRepository.java
+++ src/main/java/com/wvictoria/cars/management/persistence/crud/ViajeCrudRepository.java
@@ -3,4 +3,9 @@ package com.wvictoria.cars.management.persistence.crud;
 import com.wvictoria.cars.management.persistence.entity.Viaje;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ViajeCrudRepository extends CrudRepository<Viaje, Integer> {}
+import java.util.List;
+
+public interface ViajeCrudRepository extends CrudRepository<Viaje, Integer> {
+    List<Viaje> findByIdEmpleadoOrderByNombreAsc(int idEmpleado);
+}
+
