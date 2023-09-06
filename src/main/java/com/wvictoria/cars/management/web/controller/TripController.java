91b261fb6b352859b3397faebd17369126134110
wvictoriadev <wvictoriadev@gmail.com>
2023-09-06 00:26:44 -0500

    :computer: Implementacion Eliminar Viaje

--- src/main/java/com/wvictoria/cars/management/web/controller/TripController.java
+++ src/main/java/com/wvictoria/cars/management/web/controller/TripController.java
@@ -3,6 +3,8 @@ package com.wvictoria.cars.management.web.controller;
 import com.wvictoria.cars.management.domain.Trip;
 import com.wvictoria.cars.management.domain.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,5 +29,14 @@ public class TripController {
     @PostMapping("/save")
     public Trip save(@RequestBody Trip trip) {
         return tripService.save(trip);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable("id") int tripId) {
+        if (tripService.delete(tripId)) {
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 }
